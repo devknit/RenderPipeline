28e0140417c8170df0780db512cc6e801f56cc85
devknit <devknit.yua@gmail.com>
2020-10-05 10:46:52 +0900

    Inspectorの表示を改修

---
+++ Editor/PostProcesses/Ubar/FlipEditor.cs
@@ -0,0 +1,56 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+namespace RenderPipeline.Editor
+{
+	[CanEditMultipleObjects]
+	[CustomEditor( typeof( Flip))]
+	public sealed class FlipEditor : UnityEditor.Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			SerializedProperty sharedSettings = serializedObject.FindProperty( "sharedSettings");
+			SerializedProperty properties = serializedObject.FindProperty( "properties");
+			
+			if( sharedSettings == null || properties == null)
+			{
+				base.OnInspectorGUI();
+			}
+			else
+			{
+				serializedObject.Update();
+				
+				EditorGUILayout.PropertyField( sharedSettings, true);
+				
+				if( sharedSettings.objectReferenceValue != null)
+				{
+					var sharedSettingsObject = new SerializedObject( sharedSettings.objectReferenceValue);
+					sharedSettingsObject.Update();
+					OnPropertiesGUI( sharedSettingsObject.FindProperty( "properties"));
+					sharedSettingsObject.ApplyModifiedProperties();
+				}
+				else
+				{
+					OnPropertiesGUI( properties);
+				}
+				serializedObject.ApplyModifiedProperties();
+			}
+		}
+		void OnPropertiesGUI( SerializedProperty properties)
+		{
+			if( properties != null)
+			{
+				int depth = properties.depth + 1;
+				
+				while( properties.NextVisible( true) != false)
+				{
+					if( properties.depth == depth)
+					{
+						EditorGUILayout.PropertyField( properties, true);
+					}
+				}
+			}
+		}
+	}
+}
